ad790c21d141e911d68de88f8b85e629e4884fcb
Mohamed Ali <30309101700696@ci.menofia.edu.eg>
2025-11-28 03:50:54 +0200

    feat: integrate react-charts for visualizing sales metrics in Dashboard component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,27 +1,90 @@
-import { useEffect } from "react";
+import { Chart } from "react-charts";
 import supabase from "../supabase-client";
+import { useEffect, useState } from "react";
+
+interface Metric {
+  name: string;
+  sum: number;
+}
 
 function Dashboard() {
+  const [metrics, setMetrics] = useState<Metric[]>([]);
   const fetchData = async () => {
-    const response = await supabase
-      .from("sales_deals")
-      .select(
+    try {
+      const { error, data } = await supabase.from("sales_deals").select(
         `
     name,
-    value
+    value.sum()
     `
-      )
-      .order("value", { ascending: false })
-      .limit(1);
-    console.log(response);
+      );
+      if (error) {
+        throw error;
+      }
+      setMetrics(data);
+      console.log(data);
+    } catch (error) {
+      console.error("there is an error", error);
+    }
   };
+
   useEffect(() => {
     fetchData();
   }, []);
+
+  const chartData = [
+    {
+      data: metrics.map((m) => ({
+        primary: m.name,
+        secondary: m.sum,
+      })),
+    },
+  ];
+
+  const primaryAxis = {
+    getValue: (d: { primary: string; secondary: number }) => d.primary,
+    scaleType: "band" as const,
+    padding: 0.2,
+    position: "bottom" as const,
+  };
+
+  const secondaryAxes = [
+    {
+      getValue: (d: { primary: string; secondary: number }) => d.secondary,
+      scaleType: "linear" as const,
+      min: 0,
+      max: y_max(),
+      padding: {
+        top: 20,
+        bottom: 40,
+      },
+    },
+  ];
+
+  function y_max() {
+    if (metrics.length > 0) {
+      const maxSum = Math.max(...metrics.map((m) => m.sum));
+      return maxSum + 2000;
+    }
+    return 5000;
+  }
+
   return (
     <div className="dashboard-wrapper">
       <div className="chart-container">
         <h2>Total Sales This Quarter ($)</h2>
+        <div style={{ flex: 1 }}>
+          <Chart
+            options={{
+              data: chartData,
+              primaryAxis,
+              secondaryAxes,
+              defaultColors: ["#58d675"],
+              tooltip: {
+                show: false,
+              },
+            }}
+          />
+        </div>
       </div>
     </div>
   );
